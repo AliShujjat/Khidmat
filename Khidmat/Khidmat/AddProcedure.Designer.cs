8ad209f3d9f7dc6faa453d8201525325e2068038
AlinaQureshi <aq03798@st.habib.edu.pk>
2021-05-22 20:39:53 +0500

    added procedure screen.

---
+++ Khidmat/Khidmat/AddProcedure.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Khidmat
+{
+    partial class AddProcedure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.newProcedureText = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.ProcedureNameText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(131, 453);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(128, 52);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "Cancel";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(374, 453);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(147, 52);
+            this.SubmitButton.TabIndex = 5;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // newProcedureText
+            // 
+            this.newProcedureText.AutoSize = true;
+            this.newProcedureText.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.newProcedureText.Location = new System.Drawing.Point(185, 52);
+            this.newProcedureText.Name = "newProcedureText";
+            this.newProcedureText.Size = new System.Drawing.Size(270, 35);
+            this.newProcedureText.TabIndex = 4;
+            this.newProcedureText.Text = "Add New Procedure";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(234, 218);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 145);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(89, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Sub Procedures:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(234, 152);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(221, 27);
+            this.NameTextBox.TabIndex = 15;
+            // 
+            // ProcedureNameText
+            // 
+            this.ProcedureNameText.AutoSize = true;
+            this.ProcedureNameText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProcedureNameText.Location = new System.Drawing.Point(89, 152);
+            this.ProcedureNameText.Name = "ProcedureNameText";
+            this.ProcedureNameText.Size = new System.Drawing.Size(123, 20);
+            this.ProcedureNameText.TabIndex = 14;
+            this.ProcedureNameText.Text = "Procedure Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(92, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "(Add each in a new line)";
+            // 
+            // AddProcedure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 577);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.ProcedureNameText);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.newProcedureText);
+            this.Name = "AddProcedure";
+            this.Text = "AddProcedure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label newProcedureText;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label ProcedureNameText;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Khidmat/Khidmat/AddProcedure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Khidmat
+{
+    public partial class AddProcedure : Form
+    {
+        public AddProcedure()
+        {
+            InitializeComponent();
+        }
+    }
+}
